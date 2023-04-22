ac06cced1f8705d30494424fd9963e0fad1d390d
lizayao <lizayao95@hotmail.com>
2023-04-22 20:20:21 -0300

    ult

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import CartWidget from "../CartWidget/CartWidget"
-import {NavLink, Link} from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
+
 
 const NavBar = () => {
-    return(
-        <nav>
+    return (
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <Link to='/'>
-                <h3>Nail Store</h3>
+                <img src="Images/logo.png" alt="logo" />
             </Link>
-            <div>
-                <NavLink to={`/category/esmaltes`} className={({isActive}) => isActive ? 'ActiveOption' : 'Option'}>Esmaltes y polimeros</NavLink>
-                <NavLink to={`/category/decoracion`} className={({isActive}) => isActive ? 'ActiveOption' : 'Option'}>Decoracion</NavLink>
-                <NavLink to={`/category/cabinas`} className={({isActive}) => isActive ? 'ActiveOption' : 'Option'}>Cabinas UV/LED</NavLink>
-                <NavLink to={`/category/herramientas`} className={({isActive}) => isActive ? 'ActiveOption' : 'Option'}>Herramientas</NavLink>
-                <NavLink to={`/category/pinceles`} className={({isActive}) => isActive ? 'ActiveOption' : 'Option'}>Pinceles</NavLink>
+            <div className="container-fluid">
+                <NavLink to={`/category/esmaltes`} className={({ isActive }) => isActive ? 'ActiveOption' : 'Option'}>Esmaltes y polimeros</NavLink>
+                <NavLink to={`/category/decoracion`} className={({ isActive }) => isActive ? 'ActiveOption' : 'Option'}>Decoracion</NavLink>
+                <NavLink to={`/category/cabinas`} className={({ isActive }) => isActive ? 'ActiveOption' : 'Option'}>Cabinas UV/LED</NavLink>
+                <NavLink to={`/category/herramientas`} className={({ isActive }) => isActive ? 'ActiveOption' : 'Option'}>Herramientas</NavLink>
+                <NavLink to={`/category/pinceles`} className={({ isActive }) => isActive ? 'ActiveOption' : 'Option'}>Pinceles</NavLink>
             </div>
             <CartWidget />
         </nav>
